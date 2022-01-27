0c83f56fd830e7979a70c89a5a89e6cc6e90aeff
Vivek Shilimkar <83208989+vivek-infracloud@users.noreply.github.com>
2022-01-27 19:22:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,6 @@ resource "aws_instance" "ec2-instance" {
     #!/bin/bash
     curl https://releases.rancher.com/install-docker/20.10.sh | sh
     sudo chmod 777 /var/run/docker.sock
-    docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:${{ TF_VAR_rancher_version }}
+    docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:${var.rancher_version}
   EOF
 }
